73cfc150771c8a371f008ddcde8088388a26ede4
Kevin Sage <kaiwenphoenix@icloud.com>
2018-05-13 13:41:14 -0400

    Fixed Clock function, Start Game Button

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
 //Game Clock
 //Clock Variables
-let start = document.querySelector('.startClockBtn');
 let seconds = document.querySelector('.seconds');
 let minutes = document.querySelector('.minutes');
 let secs = 00,
   mins = 00;
 let matches = 0;
 let cards = document.querySelectorAll('.card');
+let cardCollection = [...cards];
 let moves = document.querySelector('.moves');
 let turns = 0;
 let turnedCards = [];
-//Clock Listener
+//New Game Listener
+let newGame = document.querySelector('#newGame');
+let canvas = document.querySelector('.deck');
+let shuffledDeck = [];
+document.body.onload = startGame();
+newGame.addEventListener('click', startGame);
+function startGame() {
+  turns = 0;
+  secs = 00;
+  mins = 00;
+  seconds.innerHTML = "0" + secs;
+  minutes.innerHTML = mins;
+  clearInterval(clock);
 
+  cardCollection = shuffle(cardCollection);
+  for (var i = 0; i < cards.length; i++) {
+    cardCollection.forEach(function(card) {
+      canvas.appendChild(card);
+
+    });
+    cardCollection[i].classList.remove("turn", "match", "disable")
+  }
+}
+
+function shuffle(array) {
+  console.log("shuffle");
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+console.log(array);
+    return array;
+};
 
 //Begin Game
-
+var clock;
 function begin() {
-  setInterval(function() {
+  clock = setInterval(function() {
     secs++;
     if (secs < 10) {
       seconds.innerHTML = "0" + secs;
@@ -42,18 +78,18 @@ cards.forEach(function(card) {
 //Matched
 
 function flipCard(e) {
+  if ((turns == 1) && (secs === 00) && (mins === 00)){
+    begin();
+  }
   this.classList.toggle('turn');
   turnedCards.push(this);
   console.log(turnedCards);
   if (turnedCards.length == 2) {
+      turns++;
     cards.forEach(function(card) {
       card.classList.toggle('disable-deck');
     })
     setTimeout(checkMatch, 1500);
-  }
-  turns++;
-  if (turns == 1) {
-    begin();
   }
   moves.innerHTML = turns;
 }
